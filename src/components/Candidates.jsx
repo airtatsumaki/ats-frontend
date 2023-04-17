0a17e89f16bbc584ed813238f04a15d5060de686
airtatsumaki <naz1mb20@gmail.com>
2023-04-17 12:58:47 +0100

    pulling candidates from DB

--- src/components/Candidates.jsx
+++ src/components/Candidates.jsx
@@ -1,16 +1,31 @@
 import { Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import { API } from '../api-service';
 
 function Candidates() {
-  const candidates = ["person 1", "person 2"];
+  // const candidates = ["person 1", "person 2"];
+  const [candidates, setCandidates] = useState(["person 1", "person 2"]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const can = await API.getDataFromApi("http://localhost:8080/candidates");
+        console.log("line 13");
+        console.log(can.data);
+        can ? setCandidates(can.data) : null;
+      } catch(err) {
+        console.log(err);
+      }
+    })()
+  }, []);
 
  //use useEffect to render candidates from DB
   return (
     <Container style={{paddingTop: "50px"}}>
         {candidates && candidates.map((item, index) => {
           return (
-            
               <div key={index}>
-                {item}
+                {item.name}
               </div>
           )
         })}
