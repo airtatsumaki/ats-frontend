7fca4e1c211fd644a08f1faf6ba952d4d91659fe
airtatsumaki <naz1mb20@gmail.com>
2023-04-23 20:04:22 +0100

    custom hook useForm done

--- src/components/AddRole.jsx
+++ src/components/AddRole.jsx
@@ -1,36 +1,21 @@
-import { useState } from 'react';
 import { Container, Form, Button} from 'react-bootstrap';
 import { API } from '../api-service';
-import { redirect, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import './styles/AddRole.css';
+import useForm from '../hooks/useForm';
 
 function AddRole(){
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
+  const [formData, handleInput] = useForm({
     title: "",
     description: ""
   });
-
-  function handleTitle(event){
-    const t = event.target.value;
-    setFormData(prevData => ({
-      ...prevData,
-      title: t
-    }));
-  }
-  function handleDescription(event){
-    const d = event.target.value;
-    setFormData(prevData => ({
-      ...prevData,
-      description: d
-    }));
-  }
   
   async function handleSubmit(event){
     event.preventDefault();
     // console.log(formData);
     const result = await API.postDataToApi("http://localhost:8080/roles", formData);
-    console.log(result);
+    // console.log(result);
     //make POST route in API, pos this data to database, return to homepage
     
     if (result.status == 200){
@@ -44,11 +29,11 @@ function AddRole(){
         <Form className="left centerForm col-sm-12" onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formGroupTitle">
             <Form.Label>Role title</Form.Label>
-            <Form.Control type="text" placeholder="Title" value={formData.title} onChange={handleTitle}/>
+            <Form.Control name="title" value={formData.title} onChange={handleInput} type="text" placeholder="Title" />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formGroupDesc">
             <Form.Label>Role description</Form.Label>
-            <Form.Control as="textarea" rows={3} type="text" value={formData.description} onChange={handleDescription} placeholder="Description" />
+            <Form.Control name="description" value={formData.description} onChange={handleInput} type="text" placeholder="Description" as="textarea" rows={3} />
           </Form.Group>
           <Button varient="secondary" type="submit">Submit</Button>
         </Form>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,16 @@
 import { useState } from 'react';
 
 function useForm(initValues){
-  
+  const [formData, setFormData] = useState(initValues);
+
+  function handleInput(event){
+    const { name, value } = event.target;
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  }
+  return [formData, handleInput];
 }
 
 export default useForm;
